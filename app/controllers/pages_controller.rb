14c96067c9771d071fbab8dca2fbf1353e60b330
sgrose <sgrose@sfu.ca>
2018-02-15 13:05:11 -0800

    welcome and about page designed

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,6 @@
+class PagesController < ActionController::Base
+    def welcome
+    end
+    def about
+    end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,13 @@
 Rails.application.routes.draw do
+    
+  get 'login/index'
+  root 'login#index'
+    
+  get 'pages/welcome'
+  root 'pages#welcome'
+  
+  get 'pages/about'
+  root 'pages#about'
+  
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end

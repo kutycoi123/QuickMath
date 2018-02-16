672c0c42c88997c8c9b9127a553e7b2388ea8789
tln3 <tln3@sfu.ca>
2018-02-15 23:15:43 -0800

    Clean the mess that I've made

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,6 @@
 class User < ApplicationRecord
 	has_many :notes
 	has_and_belongs_to_many :courses
-<<<<<<< HEAD
-
-=======
->>>>>>> 721266809624b699f5477df52229865e1f0e8966
 	def my_authenticate(password)
 		return self.password_digest == Digest::SHA1.hexdigest(password)  
 	end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,10 +5,6 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-<<<<<<< HEAD
-=======
-
->>>>>>> 721266809624b699f5477df52229865e1f0e8966
 user = User.create!(name: "testuser",
 				email: "testuser123@gmail.com",
 				password_digest: Digest::SHA1.hexdigest("123456"))

931548b93c5d22cdb876e4c94b5d9b88941396ac
mimeri <mario_imeri@sfu.ca>
2018-02-15 22:16:06 -0800

    Update sessions_controller.rb

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,5 +1,22 @@
-class LoginController < ApplicationController
-  def index
-  end
-end
+class SessionsController < ApplicationController
+	def new
+	end
 
+	def create
+		@user = User.find_by(email: params[:session][:email].downcase)
+		if @user && @user.authenticate(params[:session][:password])
+			log_in @user
+			params[:session][:remember_me] == "1" ? remember(@user) : forget(@user)
+			redirect_to @user
+		else
+			flash.now[:danger] = "Invalid email or password"
+			render 'new'
+		end
+	end
+	def destroy
+		if logged_in?
+			log_out
+		end
+		redirect_to home_path
+	end
+end

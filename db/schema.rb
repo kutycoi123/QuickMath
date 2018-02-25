2ea6897e8459b9361c4f5aa4ad0620c258523ae7
tln3 <tln3@sfu.ca>
2018-02-25 15:30:48 -0800

    Add validations and testing code for user model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180213231241) do
+ActiveRecord::Schema.define(version: 20180225224704) do
 
   create_table "courses", force: :cascade do |t|
     t.string "name"
@@ -41,6 +41,7 @@ ActiveRecord::Schema.define(version: 20180213231241) do
     t.string "password_digest"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
   end
 
 end

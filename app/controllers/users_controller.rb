4cda782aafbd24224b0ec4079d12e4a42a9304ec
tln3 <tln3@sfu.ca>
2018-02-25 12:37:59 -0800

    Add user info page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,18 +6,34 @@ class UsersController < ApplicationController
 		
 	end
 	def show
-		
+		# @user = User.find(params[:id])
+		# redirect_to edit_user_path(@user)
+		render 'edit'
 	end
 	def edit
-		
+		correct_user
 	end
 	def update
-		
+		# @user = User.find(params[:id])
+		@user = current_user
+		if @user && @user.update_attributes(user_params)
+			flash.now[:success] = "Update successfully"
+			redirect_to root_path
+		else
+			render 'edit'
+		end
 	end
 	private
 		def user_params
 			params.require(:user).permit(:name, :email, :password, :password_confirmation)
 		end
+		def correct_user
+	      @user = User.find(params[:id])
+		 	if !correct_user?(@user)
+		        flash.now[:danger] = "You are not allowed to change this sort of information"
+		        redirect_to root_path
+	      	end
+    	end
 		
 	
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,6 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-user = User.create!(name: "testuser",
+user1 = User.create!(name: "testuser",
 				email: "testuser123@gmail.com",
+				password: "123456", password_confirmation: "123456")
+user2 = User.create!(name: "testuser1",
+				email: "testuser1234@gmail.com",
 				password: "123456", password_confirmation: "123456")

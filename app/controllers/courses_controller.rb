83b7f073e307a87506f81909099b610d978f4acc
tln3 <tln3@sfu.ca>
2018-03-01 14:39:12 -0800

    Add views and controller actions for allowing users to create new courses

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -10,11 +10,60 @@ class CoursesController < ApplicationController
 		# end
 		# @non_user_notes = allNotes
 
-		@non_user_notes = Note.all
+		@non_user_notes = @course.notes
 		@user_notes = []
-		if @user = current_user
-			@user_notes = Note.where("user_id = ?", @user.id)
+		@user = current_user
+		if @user
+			if params[:user_id] && @user != User.find(params[:user_id])
+				#flash[:danger] = "User not found"
+				redirect_to root_path
+			end
+			if correct_user?(@user)
+				@user_notes = @non_user_notes.where("user_id = ?", @user.id)
+				@non_user_notes -= @user_notes
+			else
+				#flash[:danger] = "You are not allowed to see other people's information"
+				redirect_to user_path(@user)
+			end
+		
 		end
-		@non_user_notes -= @user_notes
+	end
+	def new
+		if !params[:user_id]
+			if current_user
+				redirect_to new_user_course_path(current_user)
+			else
+				flash[:danger] = "Please login to add courses"
+				redirect_to login_path
+			end
+		end
+	end
+	def create
+		@course = Course.find_by(name: params[:course][:name])
+		@user = User.find(params[:user_id])
+		if correct_user?(@user)
+			if @course
+				if !@user.courses.include?(@course)
+					@user.courses << @course
+					if @user.save
+						flash[:success] = "Add course successfully"
+						redirect_to user_path(@user)
+					else
+						flash.now[:danger] = "Fail to add course"
+						render 'new'
+					end
+				else
+					flash.now[:danger] = "Course is already in your list"
+					render 'new'
+				end
+			else
+				flash.now[:danger] = "Course not found"
+				render 'new'
+			end
+		else
+			flash[:danger] = "You are not allowed to see/change this information"
+			redirect_to user_path(@user)
+		end
+
 	end
 end

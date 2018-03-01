d69b107cf927b6a7d8ccf7738b94cda130a83e9e
tln3 <tln3@sfu.ca>
2018-02-28 23:32:26 -0800

    Add more credentials

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,9 +5,33 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-user1 = User.create!(name: "testuser",
+user = User.create!(name: "testuser",
 				email: "testuser123@gmail.com",
 				password: "123456", password_confirmation: "123456")
-user2 = User.create!(name: "testuser1",
+user1 = User.create!(name: "testuser1",
 				email: "testuser1234@gmail.com",
 				password: "123456", password_confirmation: "123456")
+course_1 = Course.create!(name: "CMPT 276",
+						  title: "Introduction to Software Engineering",
+						  description: "Required course for co-op")
+course_2 = Course.create!(name: "CMPT 295",
+						  title: "Introduction to operator system",
+						  description: "Required course for co-op")
+note_1 = Note.create!(course_name: "CMPT 276",
+					  title: "Lecture 1-10",
+					  description: "",
+					  user_id: 1,
+					  course_id: 1)
+note_2 = Note.create!(course_name: "CMPT 295",
+					  title: "Lecture 1-10",
+					  description: "",
+					  user_id: 1,
+					  course_id: 2)
+note_3 = Note.create!(course_name: "CMPT 276",
+					  title: "Lecture 11-20",
+					  description: "",
+					  user_id: 2,
+					  course_id: 1)
+user.courses << course_1
+user.courses << course_2
+user1.courses << course_1

769bfdf6ce5dd0db328e30c43a34b76daadd8f2a
tln3 <mimeri@sfu.ca>
2018-03-07 20:57:20 -0800

    Fix a typo mistake

--- app/models/course_folder.rb
+++ app/models/course_folder.rb
@@ -1,8 +1,8 @@
 class CourseFolder < ApplicationRecord
 	has_many :notes, :dependent => :delete_all
 	belongs_to :user
-	validates :name, presense: :true
-	validates :year, presense: :true
-	validates :term, presense: :true
-	validates :title, presense: :true
+	validates :name, presence: true
+	validates :year, presence: true
+	validates :term, presence: true
+	validates :title, presence: true
 end

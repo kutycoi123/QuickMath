d6f3b260b6006d1548d39574b264393ba43c03c7
Angad Singh Aujla <asa217@sfu.ca>
2018-03-25 11:44:48 -0700

    SFU API complete

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,40 +6,15 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-# class SfuDepAPI
-# 	include HTTParty
-# 	base_uri "http://www.sfu.ca/bin/wcm/course-outlines"
-# 	def getDepartments(term,year=Time.zone.now.year)
-# 		self.class.get("?#{year}/#{term.downcase}")
-# 	end
-# end
-#
-# class SfuCourseAPI
-# 	include HTTParty
-# 	base_uri "http://www.sfu.ca/bin/wcm/course-outlines"
-# 	def getCourses(department,term,year=Time.zone.now.year)
-# 		self.class.get("?#{year}/#{term.downcase}/#{department.downcase}")
-# 	end
-# end
-#
-# term="spring"
-# departArray = []
-# dep_api = SfuDepAPI.new
-# dep_json = dep_api.getDepartments(term)
-# dep_list = dep_json.parsed_response
-#
-# # dep_list.each do |y|
-# # 	departArray.push(y["value"])
-# # 	# course_api =	SfuCourseAPI.new
-# # 	# course_json = course_api.getCourses("#{y["value"]}",term)
-# # 	# course_list = course_json.parsed_response
-# # 	# course_list.each do |x|
-# # 	# 	Course.create!(name: "CMPT " + x["text"], title: x["title"], description: "")
-# # 	# end
-# # end
+class SfuDepAPI
+	include HTTParty
+	base_uri "http://www.sfu.ca/bin/wcm/course-outlines"
+	def getDepartments(term,year=Time.zone.now.year)
+		self.class.get("?#{year}/#{term.downcase}")
+	end
+end
 
-
-class SfuAPI
+class SfuCourseAPI
 	include HTTParty
 	base_uri "http://www.sfu.ca/bin/wcm/course-outlines"
 	def getCourses(department,term,year=Time.zone.now.year)
@@ -47,12 +22,64 @@ class SfuAPI
 	end
 end
 
-course_api=SfuAPI.new
-course_json=course_api.getCourses("cmpt","spring")
-course_list=course_json.parsed_response
-course_list.each do|x|
-	Course.create!(name: "CMPT #{x["text"]}", title: x["title"], description: "")
+term="spring"
+# departArray = []
+  # begin
+    # puts 'I am before the raise.'
+    # raise 'An error has occured.'
+    # puts 'I am after the raise.'
+empty=0
+dep_api = SfuDepAPI.new
+dep_json = dep_api.getDepartments(term)
+dep_list = dep_json.parsed_response
+dep_list.each do |y|
+	begin
+	# if empty==1
+	# 	empty=0
+	# 	next
+	# end
+	 # next if y["value"]=='bot'
+	 # next if y["value"]=='ddp'
+	 # next if y["value"]=='exch'
+ 	# rescue Exception=>e
+	#  puts "Error #{e}"
+	  # puts y["value"]
+# 	departArray.push(y["value"])
+	course_api =	SfuCourseAPI.new
+	# course_json = course_api.getCourses(y["value"],"spring")
+	course_json = course_api.getCourses("#{y["value"]}",term)
+	course_list = course_json.parsed_response
+# 	if $!
+# 	next
+# end
+	course_list.each do |x|
+		  # puts x["title"]
+		 Course.create!(name: y["text"] + x["text"], title: x["title"], description: "")
+	end
+rescue
+	next
 end
+ end
+# rescue
+# 	empty=1
+# 	retry
+# end
+
+#
+# class SfuAPI
+# 	include HTTParty
+# 	base_uri "http://www.sfu.ca/bin/wcm/course-outlines"
+# 	def getCourses(department,term,year=Time.zone.now.year)
+# 		self.class.get("?#{year}/#{term.downcase}/#{department.downcase}")
+# 	end
+# end
+#
+# course_api=SfuAPI.new
+# course_json=course_api.getCourses("BISC","spring")
+# course_list=course_json.parsed_response
+# course_list.each do|x|
+# 	Course.create!(name: "CMPT #{x["text"]}", title: x["title"], description: "")
+# end
 user = User.create!(name: "testuser",
 				email: "testuser123@gmail.com",
 				password: "123456", password_confirmation: "123456")

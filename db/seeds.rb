8384b0368bab10eeb8512893377701b2cde4f546
tln3 <tln3@sfu.ca>
2018-03-29 13:13:18 -0700

    Fix welcome page and seed database

--- db/seeds.rb
+++ db/seeds.rb
@@ -41,7 +41,13 @@ rescue
 	next
 end
  end
-
+seedUser = {"Rafid" => "rpranta@sfu.ca", "Lam Nguyen" => "tln3@sfu.ca", 
+				"Mario" => "mimeri@sfu.ca", "Angad" => "asa217@sfu.ca"}
+seedUser.each do |key, value|
+	User.create(name: key, email: value, password: "123456", password_confirmation: "123456", 
+				activated: true, 
+ 				activated_at: Time.zone.now)
+end
 # user = User.create!(name: "testuser",
 # 				email: "testuser123@gmail.com",
 # 				password: "123456", 
